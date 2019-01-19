4fae9170b26bfc4d1bdf67ff957b2deb837dedcc
Matej Strnad <matej.strnad.97@gmail.com>
2019-01-19 19:58:34 +0100

    implement basic zoom prototype

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,11 +1,14 @@
 import Two from '../Two'
-import renderTiles from './renderTiles'
+import Tile from './Tile'
 import { tiles, leaders } from '../data'
 
+const tileRadius = 30
 class Game {
   constructor(rootElement, setters) {
     // React API set methods
     this.setLeaders = setters.setLeaders
+
+    this.radius = tileRadius
 
     this.two = new Two({
       width: 1000,
@@ -13,18 +16,27 @@ class Game {
       type: 'WebGLRenderer',
     }).appendTo(rootElement)
 
-    this.radius = 30
-
-    renderTiles(this.two, tiles, this.radius)
-
-    document.addEventListener('mousewheel', this.handleWheelChange)
+    this.tiles = []
+    for (let i = 0; i < tiles.length; i++) {
+      const { x, z } = tiles[i]
+      this.tiles.push(new Tile(this.two, x, z, this.radius))
+    }
+    this.two.update()
 
     this.setLeaders(leaders)
+
+    document.addEventListener('mousewheel', this.handleWheelChange)
   }
   handleWheelChange = e => {
-    this.radius--
+    const zoomDirection = e.deltaY < 0 ? -1 : 1
 
-    renderTiles(this.two, tiles, this.radius)
+    this.radius += zoomDirection * 2
+
+    for (let i = 0; i < this.tiles.length; i++) {
+      this.tiles[i].setRadius(this.radius)
+    }
+
+    this.two.update()
   }
   cancelAlliance = playerId => {
     console.log(`Canceling alliance with ${playerId}`)
