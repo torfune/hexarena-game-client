0ff25fc608ae6e07a4482598e1ad1c96db0766ba
Matej Strnad <matej.strnad.97@gmail.com>
2019-02-20 22:35:01 +0100

    Enable setting gs url from env variable.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,14 @@
 // Always commit with value .... true
 export const REMOTE_GAMESERVER = true
 
-export const GAMESERVER_URL = REMOTE_GAMESERVER
-  ? 'http://dev.hexagor.io:8000'
-  : 'http://localhost:8000'
+let GAMESERVER_URL = 'http://localhost:8000'
+
+if (process.env.REACT_APP_GSURL) {
+  GAMESERVER_URL = process.env.REACT_APP_GSURL
+}
+
+if (REMOTE_GAMESERVER === false) {
+  GAMESERVER_URL = 'http://dev.hexagor.io:8000'
+}
+
+export { GAMESERVER_URL }
