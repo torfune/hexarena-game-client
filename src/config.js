dff9d49ae6ca4e0557edca019685c7b7bd63b855
Matej Strnad <matej.strnad.97@gmail.com>
2019-02-20 22:38:31 +0100

    Fix config.js

--- src/config.js
+++ src/config.js
@@ -3,12 +3,12 @@ export const REMOTE_GAMESERVER = true
 
 let GAMESERVER_URL = 'http://localhost:8000'
 
-if (process.env.REACT_APP_GSURL) {
-  GAMESERVER_URL = process.env.REACT_APP_GSURL
+if (REMOTE_GAMESERVER) {
+  GAMESERVER_URL = 'http://dev.hexagor.io:8000'
 }
 
-if (REMOTE_GAMESERVER === false) {
-  GAMESERVER_URL = 'http://dev.hexagor.io:8000'
+if (process.env.REACT_APP_GSURL) {
+  GAMESERVER_URL = process.env.REACT_APP_GSURL
 }
 
 export { GAMESERVER_URL }
