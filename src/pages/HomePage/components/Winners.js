3bca3a208142bae650c418f126aede882cdcc66a
Matej Strnad <matej.strnad.97@gmail.com>
2019-03-03 19:55:44 +0100

    Improve Winner style.

--- src/pages/HomePage/components/Winners.js
+++ src/pages/HomePage/components/Winners.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Heading from './Heading'
+import { BOX_SHADOW } from '../../../constants'
 
 const Container = styled.div`
   padding: 0 16px;
@@ -12,12 +13,19 @@ const List = styled.div`
   overflow-y: scroll;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 10px;
+  padding-right: 8px;
 `
 
 const Winner = styled.p`
   color: #fff;
   font-weight: 500;
-  font-size: 18px;
+  font-size: 14px;
+  background: #444;
+  box-shadow: ${BOX_SHADOW};
+  padding: 8px 4px;
+  text-align: center;
+  border-radius: 4px;
 `
 
 const Winners = ({ winners }) => (
