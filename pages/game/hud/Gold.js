a2a44e6f589fa3880edd8e37413827d3f4d523dd
Katarina Cvetkovicova <_>
2019-05-01 14:17:42 +0200

    Animate gold

--- pages/game/hud/Gold.js
+++ pages/game/hud/Gold.js
@@ -1,7 +1,8 @@
 import { HUD_SCALE } from 'constants/react'
 import { observer } from 'mobx-react-lite'
+import { useTransition, animated } from 'react-spring'
 import Header from 'components/Header'
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import store from 'store'
 import styled from 'styled-components'
 
@@ -27,21 +28,48 @@ const Content = styled.div`
 const Slots = styled.div`
   margin-top: 24px;
   display: flex;
-  justify-content: space-between;
 `
 
-const Slot = styled.img`
+const Slot = styled(animated.img)`
   height: 32px;
-  filter: ${props => (!props.isFilled ? 'grayscale(1)' : null)};
-  opacity: ${props => (!props.isFilled ? '0.4' : null)};
+  margin-right: 8px;
+  /* filter: ${props => (!props.isFilled ? 'grayscale(1)' : null)};
+  opacity: ${props => (!props.isFilled ? '0.4' : null)}; */
 `
 
 const GoldSection = () => {
-  if (store.gold === null) return null
+  // if (store.gold === null) return null
 
-  const goldIcons = []
-  for (let i = 0; i < 6; i++) {
-    goldIcons.push(i < store.gold)
+  // const goldIcons = []
+
+  // if (store.gold) {
+  //   for (let i = 0; i < store.gold; i++) {
+  //     goldIcons.push(true)
+  //   }
+  //   setGold(goldIcons)
+  // }
+
+  const [gold, setGold] = useState([])
+  const goldRef = useRef(gold)
+  goldRef.current = gold
+
+  const transitions = useTransition(gold, item => item.key, {
+    from: { transform: 'translate3d(0,-40px,0)' },
+    enter: { transform: 'translate3d(0,0px,0)' },
+    leave: { transform: 'translate3d(0,-40px,0)' },
+  })
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown)
+  }, [])
+
+  const handleKeyDown = e => {
+    if (e.key === 'ArrowUp') {
+      setGold([...goldRef.current, { key: Date.now() }])
+    }
+    if (e.key === 'ArrowDown') {
+      setGold(goldRef.current.slice(0, goldRef.current.length - 1))
+    }
   }
 
   return (
@@ -53,12 +81,8 @@ const GoldSection = () => {
       />
       <Content>
         <Slots>
-          {goldIcons.map((isFilled, index) => (
-            <Slot
-              key={index}
-              src="/static/icons/gold.svg"
-              isFilled={isFilled}
-            />
+          {transitions.map(({ props }, key) => (
+            <Slot key={key} style={props} src="/static/images/gold.svg" />
           ))}
         </Slots>
       </Content>
@@ -66,4 +90,4 @@ const GoldSection = () => {
   )
 }
 
-export default observer(GoldSection)
+export default GoldSection
