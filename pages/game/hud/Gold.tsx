c8652210a21797629648473e6500d1efa38dbd6c
Matej Strnad <matej.strnad.97@gmail.com>
2019-05-25 17:32:24 +0200

    Remove gold cap.

--- pages/game/hud/Gold.tsx
+++ pages/game/hud/Gold.tsx
@@ -15,56 +15,61 @@ const Container = styled.div`
   position: absolute;
   top: 0;
   user-select: none;
-  width: 256px;
+  width: 180px;
   transform-origin: left top;
   transform: scale(${HUD_SCALE});
   height: 128px;
 `
 
 const Content = styled.div`
+  display: flex;
   padding: 0 30px;
-  padding-bottom: 16px;
-`
-
-const Coins = styled.div`
-  margin-top: 24px;
+  align-items: center;
+  height: 80px;
+  justify-content: space-between;
 `
 
 const Coin = styled(animated.img)`
-  height: 32px;
+  height: 38px;
 `
 
-interface GoldItem {
-  key: string
-}
+const Count = styled.p`
+  margin-left: 12px;
+  font-size: 40px;
+  color: #444;
+`
+
+// interface GoldItem {
+//   key: string
+// }
 
 const GoldSection = () => {
-  const [gold, setGold] = useState<GoldItem[]>([])
-  const transitions = useTransition(gold, (item: { key: string }) => item.key, {
-    from: { transform: 'scale(2, 2)', opacity: 0 },
-    enter: { transform: 'scale(1.2, 1.2)', opacity: 1 },
-    leave: { transform: 'scale(2, 2)', opacity: 0 },
-  })
+  // const [gold, setGold] = useState<GoldItem[]>([])
+  // const transitions = useTransition(gold, (item: { key: string }) => item.key, {
+  //   from: { transform: 'scale(2, 2)', opacity: 0 },
+  //   enter: { transform: 'scale(1.2, 1.2)', opacity: 1 },
+  //   leave: { transform: 'scale(2, 2)', opacity: 0 },
+  // })
 
-  useEffect(() => {
-    const diff = store.gold - gold.length
+  // useEffect(() => {
+  //   const diff = store.gold - gold.length
 
-    if (diff > 0) {
-      const newGold = [...gold]
+  //   if (diff > 0) {
+  //     const newGold = [...gold]
 
-      for (let i = 0; i < diff; i++) {
-        newGold.push({ key: String(newGold.length) })
-      }
+  //     for (let i = 0; i < diff; i++) {
+  //       newGold.push({ key: String(newGold.length) })
+  //     }
 
-      setGold(newGold)
-    } else if (diff < 0) {
-      for (let i = 0; i < Math.abs(diff); i++) {
-        const newGold = gold
-        newGold.pop()
-        setGold(newGold)
-      }
-    }
-  }, [store.gold])
+  //     setGold(newGold)
+  //   } else if (diff < 0) {
+  //     for (let i = 0; i < Math.abs(diff); i++) {
+  //       const newGold = gold
+  //       newGold.pop()
+  //       setGold(newGold)
+  //     }
+  //   }
+  // }, [store.gold])
 
   return (
     <Container>
@@ -74,14 +79,16 @@ const GoldSection = () => {
         iconSize="22px"
       />
       <Content>
-        <Coins>
+        <Coin src="/static/icons/gold.svg " />
+        <Count>{store.gold}</Count>
+        {/* <Coins>
           {transitions.map(
             ({ item, props, key }) =>
               item && (
                 <Coin key={key} style={props} src="/static/icons/gold.svg" />
               )
           )}
-        </Coins>
+        </Coins> */}
       </Content>
     </Container>
   )
