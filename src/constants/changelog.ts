5247468a150a02111322889ce545c92b835b7f26
Matej Strnad <matej.strnad.97@gmail.com>
2019-11-06 20:18:21 +0100

    update changelog

--- src/constants/changelog.ts
+++ src/constants/changelog.ts
@@ -5,6 +5,11 @@ const changelog = [
       'Enemy Forests cannot be captured (same as mountains)',
       'Mountain capture cost: 4 => 1',
       'Forest capture cost: 3/4 => 1',
+      'Castle build time: 12s => 8s',
+      'Gold production per house: 0.25g/s => 0.5g/s',
+      'Base gold production: 6g/s => 4g/s',
+      'Forest spawnrate: 95 => 85',
+      'Mountain spawnrate: 30 => 28',
     ],
   },
 ]
